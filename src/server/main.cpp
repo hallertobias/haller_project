6b6dda084fb512e6758b6822694db76e714c3b80
hallertobias <haller.tobias@outlook.com>
2023-02-27 14:03:05 +0100

    Adding getCreds method

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,12 +1,29 @@
 #include "spdlog/spdlog.h"
 #include "asio.hpp"
 #include <chrono>
+#include <cstring>
 #include "CLI11.hpp"
 #include <string>
 
-
 using namespace std;
 using namespace asio::ip;
+
+string* getCreds(string line) {
+    char* charArr = new char[line.length()+1];
+    strcpy(charArr, line.c_str());
+    char *ptr; 
+    ptr = strtok(charArr, ";");
+    int counter = 0;
+    string* creds = new string[3];
+    while (ptr != NULL)  
+    {  
+        string s = ptr;
+        creds[counter] = s;
+        counter++;
+        ptr = strtok(NULL, ";");  
+    }  
+    return creds;
+}
 
 int main(int argc, char* argv[]) { 
     spdlog::info("Digest server started");
@@ -28,7 +45,8 @@ int main(int argc, char* argv[]) {
         if(strm) {
             string line;
             getline(strm, line);
-            cout << line << endl;
+            string* creds = getCreds(line);
+            cout << creds[0] << endl;
         } else {
             spdlog::error("Could not connect to client");
         }
