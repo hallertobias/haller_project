3998810d7deb0eef8f89c66ffb3b4246dcd084b7
hallertobias <haller.tobias@outlook.com>
2023-03-01 13:24:47 +0100

    Chaning first request to server

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include "CLI11.hpp"
 #include "../md5.cpp"
+#include <vector>
 
 using namespace std;
 using namespace asio::ip;
@@ -22,6 +23,7 @@ int main(int argc, char* argv[]) {
     app.add_option("-c, --credentials", credentials, "username;password;URI");
     CLI11_PARSE(app, argc, argv);
 
+    vector<string> creds;
     if (credentials.length() == 0) {
         fstream file;
         file.open("../credentials.txt",ios::in);
@@ -29,8 +31,9 @@ int main(int argc, char* argv[]) {
             string tp;
             while(getline(file, tp)){ 
                 string part = tp.substr(tp.find(":")+1, tp.length());
-                part = eraseSpaces(part) + ";";
-                credentials += part;
+                part = eraseSpaces(part);
+                creds.push_back(part);
+                credentials += part + ";";
             }
             file.close(); 
         }
@@ -40,14 +43,16 @@ int main(int argc, char* argv[]) {
     tcp::iostream stream("localhost", to_string(port));
 
     if (stream) {
-        stream << "LOGON;" << credentials << endl;
+        stream << "LOGON " << creds[2] << endl;
         string realm;
         getline(stream, realm);
         string nonce;
         getline(stream, nonce);
-        string ha1 = "username";
+        string ha1 = creds[0] + realm + creds[1];
+        string ha2 = "GET" + creds[2];
         MD5 md5;
-        cout << md5(ha1) << endl;
+        string response = md5(md5(ha1)+nonce+md5(ha2));
+        stream << creds[0] << "," << response << endl;
     } else {
         spdlog::error("Could not connect to server");
     }
